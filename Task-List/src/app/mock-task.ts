dede212cee19bcf0c0dc680ec4dbdf65da921e34
xMatiSDx <maty_arias@hotmail.com>
2022-09-22 22:59:01 -0300

    mockup task added

---
+++ Task-List/src/app/mock-task.ts
@@ -0,0 +1,27 @@
+import {Tasks} from "./tasks"
+export const TASKS: Tasks[] = [
+    {
+        id: 1,
+        text: "terminar el primer modulo de Angular",
+        day: 'agosto 5 a las 12:00',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: "Hacer las compras para la cena",
+        day: 'agosto 5 a las 17:00',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: "Investigar sobre Boostrap",
+        day: 'agosto 5 a las 19:00',
+        reminder: false,
+    },
+    {
+        id: 4,
+        text: "Leer mi libro favorito",
+        day: 'agosto 5 a las 21:00',
+        reminder: true,
+    },
+]
